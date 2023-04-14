eec9425d1bff0fe8f1a239171ca517e3342d9c3c
shivaji <20mh5a0502@acoe.edu.in>
2023-04-14 14:20:17 +0530

    config files modified

--- src/configurations/firebase-config.js
+++ src/configurations/firebase-config.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyA4cAwETQNZGmP5UtSd3slwMpXJ9gaU9-4",
-    authDomain: "login-system-97018.firebaseapp.com",
-    projectId: "login-system-97018",
-    storageBucket: "login-system-97018.appspot.com",
-    messagingSenderId: "162838724472",
-    appId: "1:162838724472:web:29ebd52ea25d94a7a9d28d"
+    /// api keys
 };
 
 // Initialize Firebase
